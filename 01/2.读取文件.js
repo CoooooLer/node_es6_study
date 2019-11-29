6b6faa507bf4dccb6c8d8d9831748f543a06be8d
CoooooLer <13939@qq.com>
2019-11-29 16:45:32 +0800

    es6获取文件绝对路径

---
+++ 01/2.读取文件.js
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import path from 'path'
+
+
+// const fs = require('fs') // fs系统函数
+// const path = require('path')
+
+// let fileName = path.delimiter + '/data/test.txt'
+console.log(__dirname)
+
+// fs.readFile(fileName, (err, data) => {
+//   if(err) {
+//     console.log('fail',err);
+//   } else {
+//     console.log('success', data.toString());
+//   }
+// });
